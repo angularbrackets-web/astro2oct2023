0fdabe79470b6e811eab6d40d61ac4cb12f66819
angularbrackets-web <angularbrackets.web@gmail.com>
2023-10-03 12:56:32 -0600

    added image

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,5 +1,11 @@
+
+import donateImage from "../assets/donate.jpg"
+
 export default function Test(){
     return (<div className="p-10 my-5 bg-green-500 text-gray-100">
         <h2>TEST</h2>
+        <div>
+			<img src={donateImage.src} alt="donate" />
+		</div>
     </div>)
 }
